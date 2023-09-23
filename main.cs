1d0e5cc5eaee1bd9e82997a16ea57cc326d22748
andrewDev08 <143661891+andrewDev08@users.noreply.github.com>
2023-09-23 14:22:25 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+//20230923_AndrewHorton_Exercise3.1
+class Program {
+  public static void Main (string[] args) {
+    int characterLimit = 140;
+    
+    Console.WriteLine("Please enter the tweet: ");
+    string tweet = Console.ReadLine();
+
+    if ( tweet.Length > characterLimit )
+    {
+      Console.WriteLine("Rejected");
+    }
+    else
+    {
+      Console.WriteLine("Accepted");
+    }
+  }
+}
